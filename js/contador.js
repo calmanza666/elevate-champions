32625a72366babb3a62bfb4dc4de49b11b5eaccb
Josep Romani <jromani@cuevatech.com>
2024-11-20 17:37:53 -0500

    xd 36

---
+++ js/contador.js
@@ -0,0 +1,19 @@
+function contador(fecha, elemento){
+    const [dia, mes, anio, hora, minutos] = fecha.split(/[/ :]/);
+    const timestamp = new Date(`${anio}-${mes}-${dia}T${hora}:${minutos}`).getTime();
+    var countDownDate = new Date(timestamp).getTime();
+    var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById(elemento).innerHTML = '<div class=col-3><div class="item_count text-center dias"><h3>'+days+'</h3><p>Días</div></div><div class=col-3><div class="item_count text-center horas"><h3>'+hours+'</h3><p>Horas</div></div><div class=col-3><div class="item_count text-center minutos"><h3>'+minutes+'</h3><p>Minutos</div></div><div class=col-3><div class="item_count text-center segundos"><h3>'+seconds+'</h3><p>Segundos</div></div>';
+    if (distance < 0) {
+        clearInterval(x);
+        //document.getElementById("reloj_torneo").innerHTML = "EXPIRED";
+    }
+    }, 1000);
+}
